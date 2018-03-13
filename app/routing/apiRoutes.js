f582d5a47701524d6c46f208caf725aa736fabfe
Tony Polanowski <obsideonslate@gmail.com>
2018-03-13 00:02:57 -0600

    little changes because my computer likes to stop working

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -18,7 +18,26 @@ module.exports = function(app){
         var userData = req.body;
         var userScores = userData.scores;
 
-        console.log(userScores); 
+        console.log(userScores); //This is the issue, we don't know whaqt this is.
 
-    })
+        //This variable will calculate the diffeerence between the user's score
+        //and the score of each user in the database
+        var totalDifference = 0;
+
+        //Loop through all possibilities in the database.
+        for (var i = 0; i < friends.length; i++) {
+            console.log(friends[i]);
+            totalDifference = 0;
+
+            //Then loop through all the scores of each friend
+            for (var j = 0; j < friends[i].scores[j]; j++){
+                totalDifference += Math.abs(parseInt(userScores[j])- parseInt(friends[i].scores[j]));
+
+                if (totalDifference <= bestMatch.friendDifference){
+                    
+                }
+            }
+        }
+
+    });
 }

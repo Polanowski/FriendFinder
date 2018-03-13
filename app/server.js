89a595fd5dbf6fedea9e67289f24606f737fa036
Anthony Polanowski <Polanowski@users.noreply.github.com>
2018-03-12 23:13:25 -0600

    Update server.js
    Copied the boilerplate and express code

---
+++ app/server.js
@@ -0,0 +1,24 @@
+// creating dependencies
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+var PORT = process.env.PORT || 8080;
+
+// create application/json parser
+var jsonParser = bodyParser.json()
+
+//create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({extended: false})
+
+// parse various different custom JSON types as JSON
+app.use(bodyParser.json({type: 'application/*+json'}))
+
+//parse some custom thing into a buffer
+app.use(bodyParser.raw({type: 'application/vnd.custom-type'}))
+
+app.use(bodyParser.text({type: 'text/html'}))
+
+app.listen(PORT, function(){
+    console.log("App linstening on PORT: " + PORT)
+});

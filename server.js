b5f4eae8656b191153acae1a7482aa165ae7ee7c
Anthony Polanowski <Polanowski@users.noreply.github.com>
2018-03-15 23:14:29 -0600

    Update server.js
    fixing duplicate issues

--- server.js
+++ server.js
@@ -5,11 +5,8 @@ var bodyParser = require('body-parser');
 
 var PORT = process.env.PORT || 8080;
 
-// create application/json parser
-var jsonParser = bodyParser.json()
-
 //create application/x-www-form-urlencoded parser
-var urlencodedParser = bodyParser.urlencoded({extended: false})
+app.use(bodyParser.urlencoded({extended: true}));
 
 // parse various different custom JSON types as JSON
 app.use(bodyParser.json({type: 'application/*+json'}))
